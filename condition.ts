80f1a827c969323a033dcb84b8a386f58ddc418f
surajwaje2209 <surajwaje2209@gmail.com>
2025-07-24 17:51:53 +0530

    simple problem

---
+++ condition.ts
@@ -0,0 +1,7 @@
+let age1: number = Number(prompt("Enter The Age:"));
+
+if (age1 >= 18) {
+  console.log("You're eligible for voting");
+} else {
+  console.log("You're not eligible for voting");
+}
---
+++ variable.ts
@@ -0,0 +1,3 @@
+let username: string = "Suraj";
+let age: number = 22;
+let isLoggedIn: boolean = true;
